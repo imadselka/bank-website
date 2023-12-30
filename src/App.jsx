193689a43cd45011d9a1f072401c8c0bbb0dc650
Imad Selka <imadselkawi133@gmail.com>
2023-12-30 13:03:04 +0100

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,42 @@
+import styles from "./style";
+import {
+  Navbar,
+  Hero,
+  Stats,
+  Business,
+  Billing,
+  CardDeal,
+  Testimonials,
+  Clients,
+  CTA,
+  Footer,
+} from "./components";
+
+const App = () => {
+  return (
+    <div className="bg-primary w-full overflow-hidden">
+      <div className={`${styles.paddingX} ${styles.flexCenter}`}>
+        <div className={`${styles.boxWidth}`}></div>
+        <Navbar />
+      </div>
+      <div className={`bg-primary ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}></div>
+        <Hero />
+      </div>
+      <div className={`bg-primary ${styles.paddingX} ${styles.flexStart}`}>
+        <div className={`${styles.boxWidth}`}>
+          <Stats />
+          <Billing />
+          <Business />
+          <CardDeal />
+          <Testimonials />
+          <Clients />
+          <CTA />
+          <Footer />
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default App;
